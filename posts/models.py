a677a419c9ef6db1064d250d222c8ff613cb84ed
Александра <113774024+Alexandra-vic@users.noreply.github.com>
2023-03-19 21:19:59 +0600

    cleaning code

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,27 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=100, blank=False, verbose_name='заголовок')
-    body = models.TextField(blank=False, verbose_name='поле текста')
-    date = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(
+        max_length=100, 
+        blank=False, 
+        verbose_name='заголовок',
+    )
+    body = models.TextField(
+        blank=False, 
+        verbose_name='поле текста',
+    )
+    date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_on = models.DateTimeField(
+        auto_now=True,
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
