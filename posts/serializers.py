d4646a6881784cf94a8deebe2faed5b3211b64f5
Александра <113774024+Alexandra-vic@users.noreply.github.com>
2023-03-19 21:21:45 +0600

    cleaning code

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,16 +1,28 @@
 from rest_framework import serializers
-from .models import Post
 
 from django.contrib.auth import get_user_model
+
+from posts.models import Post
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'body', 'date', 'updated_on', 'author',)
+        fields = (
+            'id', 
+            'title', 
+            'body', 
+            'date', 
+            'updated_on', 
+            'author',
+        )
         
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username',)        
+        fields = (
+            'id', 
+            'username',
+        )        
